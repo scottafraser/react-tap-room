5c076c62d11c889ae4ae3f17d0b4e59d59efab27
Scott Fraser <scottafraser@yahoo.com>
2018-09-09 23:09:57 -0700

    wont update date in list

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+// import { Switch } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styles from "./App.css";
 import FlatButton from "material-ui/FlatButton";
+import beers from "../assets/images/beers.png";
 
 function Header() {
   return (
-    <Switch>
-      <div>
-        <h1 className={styles.header}>Beers!</h1>
-        <div className={styles.nav}>
-          <Link to="/">
-            <FlatButton label="Home" default={true} />
-          </Link>
-          <Link to="/kegList">
-            <FlatButton label="Keg List" default={true} />
-          </Link>
-          <Link to="/newKegForm">
-            <FlatButton label="Add Keg" default={true} />
-          </Link>
-        </div>
+    // <Switch>
+    <div>
+      <img className={styles.beerLogo} src={beers} alt="beer" />
+      <div className={styles.nav}>
+        <Link to="/">
+          <FlatButton label="Home" default={true} />
+        </Link>
+        <Link to="/kegList">
+          <FlatButton label="Keg List" default={true} />
+        </Link>
+        <Link to="/newKegForm">
+          <FlatButton label="Add Keg" default={true} />
+        </Link>
       </div>
-    </Switch>
+    </div>
+    // </Switch>
   );
 }
 
--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,16 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./App.css";
+import { Card, CardActions, CardHeader, CardText } from "material-ui/Card";
+import FlatButton from "material-ui/FlatButton";
+import beerkeg from "../assets/images/beer-keg.png";
 
 function Keg(props) {
   return (
     <div className={styles.kegCard}>
-      <h2>{props.name}</h2>
-      <h3>{props.brewer}</h3>
-      <p>Style: {props.description}</p>
-      <p>{props.abv} ABV </p>
-      <p>$ {props.price}</p>
-      <hr />
+      <Card>
+        <CardHeader
+          title={props.name}
+          subtitle={props.brewer}
+          actAsExpander={true}
+          showExpandableButton={true}
+          avatar={beerkeg}
+        />
+        <CardActions>
+          <FlatButton label="Edit" />
+          <FlatButton label="Delete" />
+        </CardActions>
+        <CardText expandable={true}>
+          {props.description} <p> {props.abv} ABV </p>
+          <p>$ {props.price}</p>
+        </CardText>
+      </Card>
     </div>
   );
 }
